19fbc139f45362ab6c238a65062683ae7b06ff18
xyz <xyz@localhost.localdomain>
2017-07-17 00:43:05 +0200

    Exercise four solution

--- exercise_3_4.rb
+++ exercise_3_4.rb
@@ -1,7 +1,3 @@
-ahmed= [80,40,50]
-mohamed=[40,80,90]
-yasmine=[60,90,95]
-
 # Please count how many times each grade is repeated
 # So for example, the above should return 2 for 80
 # output should be a hash with the grade as the key
@@ -13,6 +9,29 @@ yasmine=[60,90,95]
 # hint order_grades(*attributes)
 # return value should be the hash.
 
+ahmed= [80,40,50]
+mohamed=[40,80,90]
+yasmine=[60,90,95]
+
+def order_grades(*args)
+
+	if args.length == 2
+		a=args[0].concat args[1]
+	elsif args.length == 3
+		a=args[0].concat args[1].concat args[2]
+	end
+
+	myhash={}
+	a.each do |s|
+		unless myhash.include?(s)
+			myhash.merge!(s => 1)
+		else
+			myhash[s] = myhash[s] + 1
+		end
+
+	end
+	myhash
+end
 
 # This should work
 puts order_grades(ahmed,mohamed,yasmine)
