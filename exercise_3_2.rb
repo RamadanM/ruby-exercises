066976c745b625a9d715bc7bbf87bf00fb81751f
xyz <xyz@localhost.localdomain>
2017-07-16 20:17:44 +0200

    Exercise two solution

--- exercise_3_2.rb
+++ exercise_3_2.rb
@@ -14,5 +14,34 @@
 
 
 class Document
+
+	attr_accessor :author
+	attr_accessor :title
+	attr_accessor :content
+
+	def initialize(options = {})
+		self.author = options[:author]
+		self.title = options[:title]
+		self.content = options[:content]
+    end
+
+	def word
+	  self.content.split(" ")
+	end
+
+	def each_word
+		yield word
+	end
+
+
 end
+
+fd=Document.new(:author => "someone", :title => "my book", :content => "this is the content of FD")
+puts fd.word
+
+document=Document.new(:author => "someone", :title => "my book", :content => "another content here ")
+
+ document.each_word do |word|
+  puts word
+ end
 
